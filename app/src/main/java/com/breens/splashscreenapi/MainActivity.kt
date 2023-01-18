4bc6bfa23fa3e1ba62b4a8c05c36e3a3748ab3fe
Breens Mbaka <breens.mbaka@dlight.com>
2023-01-18 07:40:39 +0300

    Add splash screen theme instance in main activity to set the theme of the app to the postSplashScreen theme

--- app/src/main/java/com/breens/splashscreenapi/MainActivity.kt
+++ app/src/main/java/com/breens/splashscreenapi/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.breens.splashscreenapi.ui.theme.SplashScreenAPITheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             SplashScreenAPITheme {
                 // A surface container using the 'background' color from the theme
